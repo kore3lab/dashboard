becd4f9c909b9de8cddacbe0b195719028da897d
escho <itnpeople@naver.com>
2021-01-13 17:23:08 +0900

    chore: Update docker build  & docs (#3)

--- src/app/backend/backend.go
+++ src/app/backend/backend.go
@@ -3,13 +3,19 @@
 package main
 
 import (
+	flag "github.com/spf13/pflag"
+
 	"github.com/acornsoftlab/dashboard/pkg/config"
 	"github.com/acornsoftlab/dashboard/router"
 )
 
 func main() {
+	var kubeconfig *string
 
-	config.Setup()
+	kubeconfig = flag.String("kubeconfig", "", "The path to the kubeconfig used to connect to the Kubernetes API server and the Kubelets (defaults to in-cluster config)")
+	flag.Parse()
+
+	config.Setup(*kubeconfig)
 	router.CreateUrlMappings()
 	router.Router.Run(":3001")
 
