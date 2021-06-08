a597d0b8cfcd9e543b95d1b374024ffee1c0e765
goldenbell <goldenbell@gmail.com>
2021-06-08 20:44:26 +0900

    Add: package.json terminal part add / terminal README.md add

--- src/app/frontend/pages/terminal.vue
+++ src/app/frontend/pages/terminal.vue
@@ -8,6 +8,7 @@ import { Xterm } from "../static/terminal/js/xterm";
 import { WebTTY, protocols } from "../static/terminal/js/webtty";
 import { ConnectionFactory } from "../static/terminal/js/websocket";
 import {debounce} from 'lodash';
+const axios = require('axios');
 
 export default {
 	layout: 'termlayout',
@@ -18,13 +19,27 @@ export default {
 			instterm:"",
 			requrl:"",
 			token: "",
-			debouncedFit_: null
+			debouncedFit_: null,
+			axiosInst: "",
+			reqHost: ""
 		};
 	},
 	mounted() {
 		if(!this.$route.query.termtype){
 			this.toast("request parameter(termtype) error");
 		}
+		//개발환경에서 terminalPort 설정위한 별도 axios설정
+		if (`${this.$config.nodeEnv}` === "development") {
+			this.reqHost = `${location.hostname}:${this.$config.terminalPort}`;
+			this.axiosInst = axios.create({
+				baseURL: `${location.protocol}//` + this.reqHost
+			});
+			
+		} else {
+			this.reqHost = `${location.host}`;
+			this.axiosInst = this.$axios;
+		}
+
 
 		this.elem = document.getElementById("terminal");
 		this.instterm = new Xterm(this.elem);
@@ -59,7 +74,7 @@ export default {
 					return;
 			}
 
-			this.$axios.get(this.requrl)
+			this.axiosInst.get(this.requrl)
 					.then( resp => {
 						this.token = resp.data.Token;
 						console.log(resp.data.Token);
@@ -74,14 +89,8 @@ export default {
 			const httpsEnabled = `${location.protocol}` === "https:";
 			const reqProtocol = httpsEnabled ? 'wss://' : 'ws://';
 			const reqTailURL = '/api/terminal/ws';
-            let reqHost = "";
 
-			if (`${this.$config.nodeEnv}` === "development") {
-				reqHost = `${location.hostname}:${this.$config.backendPort}`;
-			} else {
-				reqHost = `${location.host}`;
-			}
-			const url = reqProtocol + reqHost + reqTailURL;
+			const url = reqProtocol + this.reqHost + reqTailURL;
 
             let authToken = this.token;
 			let factory = new ConnectionFactory(url, protocols);
