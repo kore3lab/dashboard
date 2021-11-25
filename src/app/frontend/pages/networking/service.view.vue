89d3e617d8c80770b847bc3791abee78093c7859
escho <itnpeople@naver.com>
2021-11-25 16:38:57 +0900

    improve: external column

--- src/app/frontend/pages/networking/service.view.vue
+++ src/app/frontend/pages/networking/service.view.vue
@@ -4,10 +4,10 @@
 	<c-metadata v-model="value" dtCols="2" ddCols="10" @navigate="$emit('navigate', arguments[0])">
 		<dt class="col-sm-2">Selector</dt>
 		<dd class="col-sm-10">
-			<span v-for="(val, idx) in info.selector" v-bind:key="idx" class="border-box background">{{ val }}</span>
+			<span v-for="(val, idx) in spec.selector" v-bind:key="idx" class="border-box background">{{ val }}</span>
 		</dd>
-		<dt class="col-sm-2">Type</dt><dd class="col-sm-10">{{ info.type }}</dd>
-		<dt class="col-sm-2">Session Affinity</dt><dd class="col-sm-10">{{ info.sessionAffinity }}</dd>
+		<dt class="col-sm-2">Type</dt><dd class="col-sm-10">{{ spec.type }}</dd>
+		<dt class="col-sm-2">Session Affinity</dt><dd class="col-sm-10">{{ spec.sessionAffinity }}</dd>
 	</c-metadata>
 	<!-- 2. connection -->
 	<div class="row">
@@ -16,24 +16,32 @@
 				<div class="card-header p-2"><h3 class="card-title">Connection</h3></div>
 				<div class="card-body p-2">
 					<dl class="row mb-0">
-						<dt class="col-sm-2">Cluster IP</dt><dd class="col-sm-10">{{ connection.clusterIP }}</dd>
-						<dt class="col-sm-2">Ports</dt><dd class="col-sm-10">
-						<ul class="list-unstyled mb-0">
-							<li v-for="(val,idx) in connection.ports" v-bind:key="idx">{{ val }}</li>
-						</ul>
-					</dd>
+						<dt v-if="spec.clusterIP" class="col-sm-2">Cluster IP</dt>
+						<dd v-if="spec.clusterIP" class="col-sm-10">{{ spec.clusterIP }}</dd>
+						<dt v-if="spec.ports.length>0" class="col-sm-2">Ports</dt>
+						<dd v-if="spec.ports.length>0" class="col-sm-10">
+							<ul class="list-unstyled mb-0">
+								<li v-for="(val,idx) in spec.ports" v-bind:key="idx">{{ val }}</li>
+							</ul>
+						</dd>
+						<dt v-if="spec.externals.length>0" class="col-sm-2">Externals</dt>
+						<dd v-if="spec.externals.length>0" class="col-sm-10">
+							<ul class="list-unstyled mb-0">
+								<li v-for="(val,idx) in spec.externals" v-bind:key="idx">{{ val }}</li>
+							</ul>
+						</dd>
 					</dl>
 				</div>
 			</div>
 		</div>
 	</div>
 	<!-- 3. endpoint -->
-	<div v-if="endpoints.length>0" class="row">
+	<div v-if="spec.endpoints.length>0" class="row">
 		<div class="col-md-12">
 			<div class="card card-secondary card-outline">
 				<div class="card-header p-2"><h3 class="card-title">Endpoint</h3></div>
 				<div class="card-body p-2 overflow-auto">
-					<b-table-lite small :items="endpoints" :fields="fields" class="subset">
+					<b-table-lite small :items="spec.endpoints" :fields="fields" class="subset">
 						<template v-slot:cell(name)="data">
 							<a href="#" @click="$emit('navigate', getViewLink('', 'endpoints', data.item.namespace, data.item.name))">{{ data.value }}</a>
 						</template>
@@ -65,9 +73,11 @@ export default {
 	},
 	data() {
 		return {
-			info: [],
-			connection: [],
-			endpoints: [],
+			spec: {
+				externals: [],
+				ports: [],
+				endpoints: []
+			},
 			fields: [
 				{ key: "name", label: "Name" },
 				{ key: "endpoints", label: "Endpoints" },
@@ -80,16 +90,21 @@ export default {
 	methods: {
 		onSync(data) {
 			if(!data) return
-			this.info = {
+			this.spec = {
 				selector: data.spec.selector? this.stringifyLabels(data.spec.selector):[] ,
 				type: data.spec.type || "",
 				sessionAffinity: data.spec.sessionAffinity || 'None',
+				clusterIP: data.spec.clusterIP,
+				ports: this.toEndpointList(data.spec.ports,data.spec.type) || [],
+				externals: [],
+				endpoints: this.getEndpoints(data)
 			};
-			this.connection = {
-				clusterIP: data.spec.clusterIP || "-",
-				ports: this.toEndpointList(data.spec.ports,data.spec.type) || "",
-			};
-			this.endpoints = this.getEndpoints(data);
+			if (data.spec.type == "LoadBalancer" && data.status.loadBalancer.ingress) {
+				this.spec.externals = data.status.loadBalancer.ingress || [];
+			} else if(data.spec.type == "ExternalName") {
+				this.spec.externals = [data.spec.externalName];
+			}
+
 		},
 		getEndpoints(data) {
 			let list =[];
